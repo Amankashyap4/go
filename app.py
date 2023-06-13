81dfb82b6fc7b656d8d57085563d8ce6d5d28493
Nova User <nova@quantumgroupgh.com>
2023-06-13 11:50:04 +0530

    setup template

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -23,3 +24,7 @@ def contact():
 @app.route('/go/about')
 def about():
     return '<h1>company about us page go</h1>'
+
+@app.route('/go/index')
+def go_index():
+    return render_template('index.html')
